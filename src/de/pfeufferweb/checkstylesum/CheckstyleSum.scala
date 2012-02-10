3480f26e2b8006abf6104737e9aeabb33b1e8ee1
Rene Pfeuffer <rene@pfeuffer-web.de>
2012-02-10 11:15:05 +0100

    refac; doku

--- src/de/pfeufferweb/checkstylesum/CheckstyleSum.scala
+++ src/de/pfeufferweb/checkstylesum/CheckstyleSum.scala
@@ -4,31 +4,59 @@ import scala.xml._
 import scala.collection.mutable.Map
 import scala.collection.mutable.HashMap
 
+/**
+ * Diese Klasse liest die Meldungen aus mehreren Checkstyle-Dateien und summiert
+ * die Fehler gruppiert nach Fehlerart oder Datei.
+ * <p>
+ * Aufruf: CheckstyleSum [files|errors] <Checkstyle-Dateien>
+ */
 object CheckstyleSum {
 
-  val errorCounts = new HashMap[String, Int]
-  val fileCounts = new HashMap[String, Int]
-
   def main(args: Array[String]) {
-    args.foreach(readFile)
-    errorCounts.toList.sortBy({ _._2 }).foreach(x => println(x._2 + "\t" + x._1))
-    println(fileCounts.values.foldLeft(0)((sum, c) => {sum + c}))
+    val counter = if (args(0) == "files") new ByFileCounter else new ByErrorCounter
+    args.drop(1).foreach(readFile(counter, _))
+    printCounts(counter.counts)
+    printSum(counter.counts)
   }
 
-  private def readFile(checkstyleFile: String) {
+  private def readFile(counter: Counter, checkstyleFile: String) {
     val data = XML.loadFile(checkstyleFile)
-    (data \\ "file") foreach { file =>
-      (file \\ "error") foreach incError
-      incFile(file)
+    counter process data
+  }
+
+  private def printCounts(counts: Map[String, Int]) =
+    counts.toList.sortBy({ _._2 }).foreach(x => println(x._2 + "\t" + x._1))
+
+  private def printSum(counts: Map[String, Int]) =
+    println(counts.values.foldLeft(0)((sum, c) => { sum + c }))
+
+}
+
+private abstract class Counter {
+
+  val counts = new HashMap[String, Int]
+
+  def readFile(checkstyleFile: String) = process(XML.loadFile(checkstyleFile))
+
+  def process(data: Elem)
+
+  def inc(s: String, n: Int = 1) = counts += Pair(s, counts.get(s).getOrElse(0) + n)
+}
+
+private class ByFileCounter extends Counter {
+
+  def process(data: Elem) = (data \\ "file").foreach(incFile(_))
+
+  private def incFile(file: Node) = inc((file \\ "@name").text, (file \\ "error").size)
+}
+
+private class ByErrorCounter extends Counter {
+
+  def process(data: Elem) = {
+    (data \\ "file") foreach {
+      file => (file \\ "error") foreach incError
     }
   }
 
-  private def incError(entry: Node) =
-    inc(errorCounts, (entry \\ "@source").text)
-
-  private def incFile(file: Node) =
-    inc(fileCounts, (file \\ "@name").text, (file \\ "error").size)
-
-  private def inc(map: Map[String, Int], s: String, n: Int = 1) =
-    map += Pair(s, map.get(s).getOrElse(0) + n)
+  private def incError(entry: Node) = inc((entry \\ "@source").text)
 }
--- src/de/pfeufferweb/checkstylesum/CheckstyleSuppress.scala
+++ src/de/pfeufferweb/checkstylesum/CheckstyleSuppress.scala
@@ -2,33 +2,31 @@ package de.pfeufferweb.checkstylesum
 
 import scala.xml._
 
+/**
+ * Diese Klasse liest die gegebenen Checkstyle-Ergebnisdateien und erstellt daraus
+ * eine Checkstyle-konforme XML-Suppressions-Datei für alle gefundenen Fehler.
+ */
 object CheckstyleSuppress {
 
-  def main(args: Array[String]):Unit = {
-    println(new PrettyPrinter(800, 4).format(generateXml(args)))
-  }
-  
+  def main(args: Array[String]) = println(new PrettyPrinter(800, 4).format(generateXml(args)))
+
   private def generateXml(files: Array[String]) = {
-    <suppressions>{
-    files map readFile
-    }</suppressions>
+    <suppressions>{ files map readFile }</suppressions>
   }
 
   private def readFile(checkstyleFile: String) = {
     val data = XML.loadFile(checkstyleFile)
     (data \\ "file").map(createSuppressionsForFileNode(_))
   }
-  
-  private def createSuppressionsForFileNode(file: Node) = {
+
+  private def createSuppressionsForFileNode(file: Node) =
     (file \\ "error").map(suppress(extractFileName(file), _))
-  }
-  
+
   private def extractFileName(file: Node) = {
     val absoluteFileName = (file \\ "@name").text
     absoluteFileName.substring(absoluteFileName.lastIndexOf("\\") + 1)
   }
-  
-  private def suppress(file: String, error: Node) = {
-    <suppress checks={(error \\ "@source").text} files={file} lines={(error \\ "@line").text} />
-  }
+
+  private def suppress(file: String, error: Node) = 
+    <suppress checks={ (error \\ "@source").text } files={ file } lines={ (error \\ "@line").text }/>
 }
